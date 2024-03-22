3dd3884433489529b59e2369a57f1a92239c1c7e
andregirao05 <andregirao05@gmail.com>
2024-03-22 17:28:41 -0400

    adiciona uma consulta no script sql

--- sql_scripts/consultas.sql
+++ sql_scripts/consultas.sql
@@ -1,3 +1,17 @@
 USE livraria;
 
-select * from editora;
+-- a) (SELECT) Listar os nomes de todos os autores que têm edições de seus
+--    livros publicados com uma determinada editora (id da editora dado como
+--    entrada).
+SELECT DISTINCT  
+ autor.nome
+FROM autor
+JOIN escreve
+	ON autor.codigo = escreve.codigo_autor
+JOIN livro 
+    ON escreve.codigo_livro = livro.codigo
+JOIN edicao
+    ON livro.codigo = edicao.codigo_livro
+JOIN editora
+    ON editora.codigo = edicao.codigo_editora
+WHERE editora.codigo = 51;
